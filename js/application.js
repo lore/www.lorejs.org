6b262a4532f1109e1d7eac770553003360df527d
Jason Hansen <jason_hansen@outlook.com>
2016-05-17 23:21:49 -0700

    set defaults for pretty-embed

--- js/application.js
+++ js/application.js
@@ -90,5 +90,11 @@ $(function () {
   // Load prettyembed.js to make pretty YouTube videos
   $().prettyEmbed({
     // useFitVids: true
+    showInfo: false,
+    showControls: true,
+    loop: false,
+
+    colorScheme: 'dark',
+    showRelated: false
   });
 });
